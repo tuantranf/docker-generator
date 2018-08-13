02f32c3657cf7e6db253c33aa15619689dfe70d4
Tuan Tran <tuantranf@gmail.com>
2018-08-13 07:53:23 +0700

    :recycle: restructure code

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+MAINTAINER 
+
+FROM php7:0-jessie
+
+RUN apt-get -y update
+RUN apt-get -y install test testdd
+
+
+}
+
--- cli.py
+++ cli.py
@@ -1,12 +1,64 @@
+import os
 import click
 
 from generator.context import pass_context
-from generator.generator import Generator
+from generator.file import generate_docker_file
 
-@click.command(cls=Generator)
-@click.option('-v', '--verbose', is_flag=True, help='Enable verbose mode.')
+@click.command('new', short_help='Generate new docker setup.')
+@click.option('--dryrun', required=False, default=False, type=bool)
+@click.option('--name',
+              prompt='Project\'s name',
+              default="sample",
+              help='Project\'s name')
+@click.option('--maintainer',
+              prompt='The maintainer\'s name',
+              default=lambda: os.environ.get('USER', ''),
+              help='The maintainer\'s name'
+              )
+@click.option('--email',
+              prompt='The developer\'s email address',
+              default="test@test.com",
+              help='The developer\'s email address'
+              )
+# TODO show a select list here
+@click.option('--language',
+              prompt='Programming language',
+              default="PHP",
+              help='Programming language'
+              )
+# TODO using dynamic options here @see https://github.com/pallets/click/issues/72
+@click.option('--version',
+              prompt='PHP version',
+              default="5.6",
+              help='PHP version'
+              )
+@click.argument('path', required=False, default=".", type=click.Path(resolve_path=True))
 
 @pass_context
-def cli(ctx, verbose):
-    """A generator command line interface."""
-    ctx.verbose = verbose
+def cli(
+        ctx,
+        path,
+        dryrun,
+        name,
+        maintainer,
+        email,
+        language,
+        version
+   ):
+    """Initializes a repository."""
+    if path is None:
+        path = ctx.home
+
+    ctx.log('Initialized docker setup in %s', click.format_filename(path))
+    ctx.log('Dryrun %s', dryrun)
+    ctx.log('Project name %s', name)
+    ctx.log('Maintainer %s', maintainer)
+    ctx.log('Email in %s', email)
+    ctx.log('Language %s', language)
+    ctx.log('Version %s', version)
+
+    language = language.lower();
+    if language == 'php':
+        generate_docker_file(path, name, language, version)
+    click.echo('Create ./Dockerfile')
+
